67233279b7534b100504a63646756488acde77a7
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-12-19 18:16:40 -0300

    test: add test for when event is in review

--- src/tests/check-last-event-status.spec.ts
+++ src/tests/check-last-event-status.spec.ts
@@ -4,12 +4,16 @@ class CheckLastEventStatus {
   constructor(private readonly repository: LastEventRepository) {}
   async execute(input: {groupId: string}): Promise<EventStatus> {
     const event = await this.repository.getLastEvent({groupId: input.groupId});
-    return event === undefined ? EventStatus.CLOSED : EventStatus.ACTIVE;
+    if (event === undefined) return EventStatus.CLOSED;
+    const now = new Date();
+    if (event.endDate >= now) return EventStatus.ACTIVE;
+    return EventStatus.IN_REVIEW;
   }
 }
 
 interface IEvent {
   endDate: Date;
+  reviewTimeInHours: number;
 }
 
 interface LastEventRepository {
@@ -45,6 +49,7 @@ const makeSut = (): SutOutput => {
 
 // new Date().getTime() + 1 => NOW + 1 (in the future)
 // new Date().getTime() => NOW (same time)
+// new Date().getTime() - 1 => NOW - 1 (in the past)
 
 describe('CheckLastEventStatus use case', () => {
   const groupId = 'any-group-id';
@@ -85,7 +90,8 @@ describe('CheckLastEventStatus use case', () => {
   it('should return status active if event has not ended', async () => {
     const {sut, lastEventRepositorySpy} = makeSut();
     lastEventRepositorySpy.output = {
-      endDate: new Date(new Date().getTime() + 1)
+      endDate: new Date(new Date().getTime() + 1),
+      reviewTimeInHours: 1,
     };
     
     const result = await sut.execute({groupId});
@@ -96,11 +102,24 @@ describe('CheckLastEventStatus use case', () => {
   it('should return status active if event end time is the same as now', async () => {
     const {sut, lastEventRepositorySpy} = makeSut();
     lastEventRepositorySpy.output = {
-      endDate: new Date(new Date().getTime())
+      endDate: new Date(new Date().getTime()),
+      reviewTimeInHours: 1,
     };
     
     const result = await sut.execute({groupId});
     
     expect(result).toBe(EventStatus.ACTIVE);
+  });
+
+  it('should return status in_review if event is during review time', async () => {
+    const {sut, lastEventRepositorySpy} = makeSut();
+    lastEventRepositorySpy.output = {
+      endDate: new Date(new Date().getTime() - 1),
+      reviewTimeInHours: 1
+    };
+    
+    const result = await sut.execute({groupId});
+    
+    expect(result).toBe(EventStatus.IN_REVIEW);
   });
 });
